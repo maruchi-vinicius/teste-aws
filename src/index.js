ad00cd3a428f843b3edf8807d79a1d7b3aacbb8e
maruchi-vinicius <maruchi.vinicius@gmail.com>
2023-01-07 15:38:44 -0300

    teste3 auth

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import config from './aws-exports';
+import config from './aws-exports.js';
 import { Amplify } from 'aws-amplify';
 
 Amplify.configure(config);
